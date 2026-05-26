b888ac5c9661047c83a80fbbe5c51061f5ca7700
sergeisvidlo4 <sergeisvidlo4@gmail.com>
2026-05-26 19:06:02 +0300

    Sol

--- src/App.tsx
+++ src/App.tsx
@@ -8,55 +8,53 @@ import { HomePage } from './pages/HomePage';
 import { TabsPage } from './pages/TabsPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 
-const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
-
 export const App = () => {
-
   const { pathname } = useLocation();
 
   return (
-  <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <Link to="/"
-          className={pathname === '/' ? 'navbar-item is-active' : 'navbar-item'}
-          >
-            Home
-          </Link>
-          <Link to="/tabs"
-          className={pathname.startsWith('/tabs') ? 'navbar-item is-active' : 'navbar-item'}
-          >
-            Tabs
-          </Link>
+    <>
+      {/* Also requires <html class="has-navbar-fixed-top"> */}
+      <nav
+        className="navbar is-light is-fixed-top is-mobile has-shadow"
+        data-cy="Nav"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <Link
+              to="/"
+              className={
+                pathname === '/' ? 'navbar-item is-active' : 'navbar-item'
+              }
+            >
+              Home
+            </Link>
+            <Link
+              to="/tabs"
+              className={
+                pathname.startsWith('/tabs')
+                  ? 'navbar-item is-active'
+                  : 'navbar-item'
+              }
+            >
+              Tabs
+            </Link>
+          </div>
+        </div>
+      </nav>
+
+      <div className="section">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<Navigate to="/" replace />} />
+            <Route path="/tabs">
+              <Route index element={<TabsPage />} />
+              <Route path=":tabId" element={<TabsPage />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
         </div>
       </div>
-    </nav>
-
-    <div className="section">
-      <div className="container">
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path="/home" element={<Navigate to="/" replace />} />
-          <Route path='/tabs'>
-            <Route index element={<TabsPage />} />
-            <Route path=":tabId" element={<TabsPage />} />
-          </Route>
-          <Route path='*' element={<NotFoundPage />} />
-        </Routes>
-
-
-
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
 };
